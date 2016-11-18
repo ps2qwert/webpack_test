a4e2df66fdcd9c48d3cf0146084c6951d0783499
彭康 <pk@minizhen.com>
2016-11-18 12:51:47 +0800

    webpack模版

---
+++ app/index.js
@@ -0,0 +1,10 @@
+require('../static/css/reset.css'); //加载初始化样式
+require('../static/css/style.css'); //加载组件样式
+var $ = require("n-zepto");
+
+
+
+alert("111")
+$(".main").click(function(){
+	alert("111")
+})
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var webpack = require('webpack');
+var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+ 
+module.exports = {
+    //插件项
+    plugins: [
+    	commonsPlugin,
+	new webpack.ProvidePlugin({
+	    $: 'n-zepto',
+	    Zepto: 'n-zepto',
+	    'window.Zepto': 'n-zepto',
+	    'window.$': 'n-zepto',
+	  })
+    ],
+    //页面入口文件配置
+    entry: {
+        index : './app/index.js'
+    },
+    //入口文件输出配置
+    output: {
+        path: 'dist/',
+        publicPath:'dist/',
+        filename: 'build.js'
+    },
+    module: {
+        //加载器配置
+        loaders: [
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.js$/, loader: 'jsx-loader?harmony' },
+            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+ //            {
+	//   test: require.resolve('webpack-zepto'),  // 此loader配置项的目标是NPM中的jquery
+	//   loader: 'expose?$!expose?Zepto', // 先把jQuery对象声明成为全局变量`jQuery`，再通过管道进一步又声明成为全局变量`$`
+	// }
+        ]
+    },
+    //其它解决方案配置
+    resolve: {
+        root: 'E:/github/flux-example/src', //绝对路径
+        extensions: ['', '.js', '.json', '.scss'],
+        alias: {
+            AppStore : 'js/stores/AppStores.js',
+            ActionType : 'js/actions/ActionType.js',
+            AppAction : 'js/actions/AppAction.js'
+        }
+    }
+
+};
